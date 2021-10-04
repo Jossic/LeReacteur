033d6296727188ddb114056ea8306ccb745c953b
Jossic <jossic.lapierre@gmail.com>
2021-10-04 13:41:51 +0200

    Exo check-nums OK

---
+++ JavaScript/01/check-nums.js
@@ -0,0 +1,11 @@
+
+const num1 = 68
+const num2 = 67
+
+if (num2 > num1) {
+    console.log(`num2 est supérieur à num1`);
+} else if ( num2 < num1) {
+    console.log(`num2 est inférieur à num1`);
+} else {
+    console.log(`num1 et num2 sont équivalents`);
+}
