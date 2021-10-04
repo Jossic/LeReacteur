7b295c9aabab5d12dbce40ac602f77e4708c8197
Jossic <jossic.lapierre@gmail.com>
2021-10-04 13:31:45 +0200

    Exo Apollo11 OK

---
+++ JavaScript/01/helloWord.js
@@ -0,0 +1,17 @@
+let count = 12
+
+for (let count = 12; count >=0; count--) {
+    
+    // console.log(count);
+    if (count === 8) {
+        console.log("Ignition sequence start");
+    }  else if (count === 7) {
+   
+    } else {
+        console.log(count);
+    }
+
+}
+
+console.log("All engines running");
+console.log("Liftoff! 🚀");
