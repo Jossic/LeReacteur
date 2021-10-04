f003c8be7ecaef107e708b32ca0d69c495f0b3ec
Jossic <jossic.lapierre@gmail.com>
2021-10-04 13:35:17 +0200

    Exo even-only OK

---
+++ JavaScript/01/even-only.js
@@ -0,0 +1,12 @@
+const limit = 20;
+
+// début de votre code
+for (let index = 1; index <= limit; index++) {
+    if (index%2===0) {
+        console.log(index);
+    }
+    
+}
+
+
+// fin de votre code
