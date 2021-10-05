65c866f964c1eef545194a46bb94a1b85ba58a51
Jossic <jossic.lapierre@gmail.com>
2021-10-05 10:06:37 +0200

    Modifs exo apollo11

--- JavaScript/01/apollo11.js
+++ JavaScript/01/apollo11.js
@@ -1,16 +1,9 @@
-let count = 12
-
 for (let count = 12; count >=0; count--) {
-    
-    // console.log(count);
     if (count === 8) {
         console.log("Ignition sequence start");
-    }  else if (count === 7) {
-   
-    } else {
+    }  else if (count !== 7) {
         console.log(count);
-    }
-
+    } 
 }
 
 console.log("All engines running");
