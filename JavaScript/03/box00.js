227afdc076bccaa48205de46588e349d48a878ac
Jossic <jossic.lapierre@gmail.com>
2021-10-06 13:37:50 +0200

    Exo box00 ok

---
+++ JavaScript/03/box00.js
@@ -0,0 +1,26 @@
+const box = (width, height) => {
+	let str = '';
+
+	for (let i = 0; i < height; i++) {
+		str += '\n';
+		for (let j = 0; j < width; j++) {
+			if (i === height - 1 || i === 0) {
+				if (j === width - 1 || j === 0) {
+					str += 'o';
+				} else {
+					str += '-';
+				}
+			} else {
+				if (j === width - 1 || j === 0) {
+					str += '|';
+				} else {
+					str += ' ';
+				}
+			}
+		}
+	}
+
+	return str;
+};
+
+console.log(box(10, 10));
