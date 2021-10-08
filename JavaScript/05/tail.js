64f1783085d708457a422a3c7b2e819a6c9ee369
Jossic <jossic.lapierre@gmail.com>
2021-10-08 13:28:30 +0200

    Exo tail OK

---
+++ JavaScript/05/tail.js
@@ -0,0 +1,20 @@
+/*
+  Description :
+  Gets all but the first element of array.
+
+  Arguments :
+  1) The array to query.
+
+  Returns :
+  The slice of array.
+*/
+
+// Début de votre code
+const tail = (array) => {
+	array.shift();
+	return array;
+};
+
+// Fin de votre code
+
+console.log(tail([1, 2, 3])); // Doit afficher `[2, 3]`
