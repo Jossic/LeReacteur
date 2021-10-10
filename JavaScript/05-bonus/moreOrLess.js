9594e677842a7e7ae23edc5f3a9f23867442f226
Jossic <jossic.lapierre@gmail.com>
2021-10-10 13:20:42 +0200

    Exo moreOrLess ok

--- JavaScript/05-bonus/moreOrLess.js
+++ JavaScript/05-bonus/moreOrLess.js
@@ -1,9 +1,10 @@
 const readline = require('readline'); // pour avoir accès au module `readline`
 const rl = readline.createInterface(process.stdin, process.stdout); // précise l'interface d'entrée et l'interface de sortie. stdin et stdout représentent le terminal qui exécute le programme.
-
+let num;
+let count = 0;
 const initGame = (params) => {
-	let count = 1;
-	const num = Math.floor(Math.random() * 100) + 1;
+	count = 1;
+	num = Math.floor(Math.random() * 100) + 1;
 };
 
 initGame();
@@ -16,12 +17,25 @@ const moreOrLess = () => {
 			console.log(
 				`Bravo, vous avez trouvé le nombre mystère en ${count} coups !`
 			);
-			rl.close();
+
+			rl.question(
+				'Souhaitez-vous recommancer ? (yes - no) ',
+				(answer) => {
+					if (answer === 'yes') {
+						initGame();
+						console.log('Nouveau numéro généré...');
+						rl.question('Quel est le nombre mystère ? ', onAnswer);
+					} else {
+						console.log(`Merci d'avoir joué !`);
+						rl.close();
+					}
+				}
+			);
 		} else if (Number(answer) > num) {
 			count++;
 			console.log("C'est moins !");
 			rl.question('Quel est le nombre mystère ? ', onAnswer);
-		} else {
+		} else if (Number(answer) < num) {
 			count++;
 			console.log("C'est plus !");
 			rl.question('Quel est le nombre mystère ? ', onAnswer);
