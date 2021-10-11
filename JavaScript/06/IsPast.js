2a8249902374e78ccddaaf7c42facd5b2eb96e37
Jossic <jossic.lapierre@gmail.com>
2021-10-11 16:07:43 +0200

    Fin exo isPast

--- JavaScript/06/IsPast.js
+++ JavaScript/06/IsPast.js
@@ -1,2 +1,11 @@
+const isPast = (dateJour, date) => {
+	const dateConvert = new Date(date);
+	if (dateJour.getTime() > dateConvert.getTime()) {
+		return 'Date is past';
+	} else {
+		return 'Date is not past';
+	}
+};
+
 console.log(isPast(new Date(), 'July, 14, 2019')); // Date is past
 console.log(isPast(new Date(), 'July, 14, 2025')); // Date is not past
