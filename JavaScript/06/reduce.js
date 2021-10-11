2f8e9d3778f1d2005fcd232c9b7710cee5d58869
Jossic <jossic.lapierre@gmail.com>
2021-10-11 15:40:10 +0200

    Fin exo reduce

--- JavaScript/06/reduce.js
+++ JavaScript/06/reduce.js
@@ -11,10 +11,17 @@
 */
 
 // Début de votre code
+const reduce = (array, func) => {
+	let count = 0;
+	for (let i = 1; i <= array.length - 1; i++) {
+		count += func(array[i - 1], array[i]) - array[i - 1];
+	}
+	return count;
+};
 
 // Fin de votre code
 
-const result = reduce([0, 1, 2, 3, 4], (previousValue, currentValue) => {
+const result = reduce([0, 1, 2, 3, 4, 5], (previousValue, currentValue) => {
 	return previousValue + currentValue;
 });
 
