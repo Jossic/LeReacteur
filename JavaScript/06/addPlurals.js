d13e75e257434eb77410c0700ddb8f56233e2c40
Jossic <jossic.lapierre@gmail.com>
2021-10-11 19:35:41 +0200

    Fin exo addPlurals

--- JavaScript/06/addPlurals.js
+++ JavaScript/06/addPlurals.js
@@ -1,6 +1,19 @@
-INPUT: ['cheval', 'sport', 'animal', 'formateur', 'journal'];
-OUPUT: ['chevaux', 'sports', 'animaux', 'formateurs', 'journaux'];
+const addPlurals = (array) => {
+	const input = ['cheval', 'sport', 'animal', 'formateur', 'journal'];
+	const output = ['chevaux', 'sports', 'animaux', 'formateurs', 'journaux'];
+	let index = (i) => input.indexOf(i);
+	let translate = (tr) => (index(tr) > -1 ? output[index(tr)] : tr);
+	return array.map(translate);
+};
 
 const words = ['cheval', 'sport', 'animal', 'formateur', 'journal'];
 
 console.log(addPlurals(words));
+
+// const rot13 = (str) => {
+// 	const input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+// 	const output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+// 	let index = (i) => input.indexOf(i);
+// 	let translate = (tr) => (index(tr) > -1 ? output[index(tr)] : tr);
+// 	return str.split('').map(translate).join('');
+// };
