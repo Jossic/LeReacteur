5e0b0e3395cb3dd9a85a7e5a83b6382e964ba5da
Jossic <jossic.lapierre@gmail.com>
2021-10-11 17:43:00 +0200

    Fin exo Backspace

--- JavaScript/06/Backspace.js
+++ JavaScript/06/Backspace.js
@@ -1,3 +1,20 @@
+const backspace = (str) => {
+	let reverseStr = str.split('').reverse().join('');
+	let newStr = '';
+	for (let i = 0; i < reverseStr.length; i++) {
+		if (reverseStr[i] === '@') {
+			if (reverseStr[i + 1] === '@') {
+				i = i + 3;
+			} else {
+				i = i + 1;
+			}
+		} else {
+			newStr += reverseStr[i];
+		}
+	}
+	return newStr.split('').reverse().join('');
+};
+
 console.log(backspace('Rer@ayu@@ctt@eupm@@r'));
 // Must returns "Reacteur"
 
