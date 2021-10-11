a693821df8df1ea146a383a6a5734a281a47b993
Jossic <jossic.lapierre@gmail.com>
2021-10-11 16:57:58 +0200

    Fin exo RatingStar

--- JavaScript/06/Ratingstar.js
+++ JavaScript/06/Ratingstar.js
@@ -1,20 +1,54 @@
+let etoile = '★';
+let demi = '✪';
+let sansEtoile = '☆';
+
+const ratingStar = (num) => {
+	if (isNaN(num) || typeof num !== 'number') {
+		return 'Please provide a number.';
+	} else {
+		if (num === 0) {
+			return '☆☆☆☆☆';
+		} else if (num === 0.5) {
+			return '✪☆☆☆☆';
+		} else if (num === 1) {
+			return '★☆☆☆☆';
+		} else if (num === 1.5) {
+			return '★✪☆☆☆';
+		} else if (num === 2) {
+			return '★★☆☆☆';
+		} else if (num === 2.5) {
+			return '★★✪☆☆';
+		} else if (num === 3) {
+			return '★★★☆☆';
+		} else if (num === 3.5) {
+			return '★★★✪☆';
+		} else if (num === 4) {
+			return '★★★★☆';
+		} else if (num === 4.5) {
+			return '★★★★✪';
+		} else if (num === 5) {
+			return '★★★★★';
+		}
+	}
+};
+
 console.log(ratingStar(5));
-// Must returns "★★★★★"
+// Must return "★★★★★"
 
 console.log(ratingStar(3));
-// Must returns "★★★☆☆"
+// Must return "★★★☆☆"
 
 console.log(ratingStar(2.5));
-// Must returns "★★✪☆☆"
+// Must return "★★✪☆☆"
 
 console.log(ratingStar(4.5));
-// Must returns "★★★★✪"
+// Must return "★★★★✪"
 
 console.log(ratingStar(0));
-// Must returns "☆☆☆☆☆"
+// Must return "☆☆☆☆☆"
 
 console.log(ratingStar());
-// Must returns "Please provide a number."
+// Must return "Please provide a number."
 
 console.log(ratingStar('3'));
-// Must returns "Please provide a number."
+// Must return "Please provide a number."
