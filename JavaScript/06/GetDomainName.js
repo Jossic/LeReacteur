094ddeff68a6cc96d734cf919f9d7e4fd7e70168
Jossic <jossic.lapierre@gmail.com>
2021-10-11 17:48:08 +0200

    Fin exo GetDomainName

--- JavaScript/06/GetDomainName.js
+++ JavaScript/06/GetDomainName.js
@@ -1,5 +1,13 @@
+const getDomainName = (url) => {
+	let premierPoint = url.indexOf('.');
+	let dernierPoint = url.lastIndexOf('.');
+	if (!url) {
+		return 'Please return an URL.';
+	} else return url.slice(premierPoint + 1, dernierPoint);
+};
+
 console.log(getDomainName('https://www.lereacteur.io'));
 console.log(getDomainName('https://www.twitch.tv/stevenpersia'));
 console.log(getDomainName('http://www.oldwebsite.org/'));
 console.log(getDomainName('www.google.com'));
-console.log(getDomainName());
+// console.log(getDomainName());
