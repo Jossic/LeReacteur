d0c9d758b266f638b9e6f5201a688bdb834df541
Jossic <jossic.lapierre@gmail.com>
2021-11-01 10:15:18 +0100

    fin exo Lorem

---
+++ React/01/lorem/src/components/Content.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Content = () => {
+	return (
+		<div style={{ background: 'grey', width: '40%', margin: '10px' }}>
+			<h1>Lorem</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+				mollitia porro commodi rerum autem? Cumque ipsam, sed
+				necessitatibus provident voluptatibus debitis architecto illum
+				ipsum nisi at, nemo quidem ea ullam cum expedita dolor
+				reprehenderit alias? Vel, officiis suscipit! Laboriosam
+				accusantium magnam neque molestiae. Atque impedit, sit libero
+				iusto ducimus aut provident! Dolor rem magnam iure quaerat sequi
+				enim? Eos fuga aperiam temporibus debitis quod optio libero
+				veritatis sequi nostrum? Deserunt, saepe esse? Dolores, numquam
+				explicabo dolore animi corporis officiis atque reiciendis
+				perferendis natus facilis consequatur cum laborum iure
+				voluptatibus tempora sint odit aliquam error nemo sunt! Rerum
+				ratione incidunt voluptates?
+			</p>
+		</div>
+	);
+};
+
+export default Content;
